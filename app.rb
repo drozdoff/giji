e43cdb96074806348e5a3e4321ca004b22d4fdd5
Nick Mamonov <codename.is.one@gmail.com>
2016-11-29 23:34:12 +0400

    init

---
+++ app.rb
@@ -0,0 +1,9 @@
+require 'rubygems'
+require 'bundler'
+require 'sinatra'
+require 'json'
+
+post '/payload' do
+  push = JSON.parse(request.body.read)
+  puts "I got some JSON: #{push.inspect}"
+end
